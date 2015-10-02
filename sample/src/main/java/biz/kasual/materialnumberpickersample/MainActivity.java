f01d0c18a1af572b04a31a7d1e4d7c553f78745f
Stephen Vinouze <s.vinouze@gmail.com>
2015-10-02 12:41:35 +0200

    [*] Improve sample project and update images

--- sample/src/main/java/biz/kasual/materialnumberpickersample/MainActivity.java
+++ sample/src/main/java/biz/kasual/materialnumberpickersample/MainActivity.java
@@ -1,6 +1,8 @@
 package biz.kasual.materialnumberpickersample;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -37,10 +39,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        NumberPicker picker = null;
-
+        final NumberPicker picker;
+        String alertTitle = null;
 
         if (v.equals(mDefaultButton)) {
+            alertTitle = getString(R.string.alert_default_title);
+
             picker = new NumberPicker(this);
             picker.setMinValue(1);
             picker.setMaxValue(10);
@@ -48,7 +52,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else {
             MaterialNumberPicker.Builder numberPickerBuilder = new MaterialNumberPicker.Builder(this);
 
-            if (v.equals(mCustomButton)) {
+            if (v.equals(mSimpleButton)) {
+                alertTitle = getString(R.string.alert_simple_title);
+            }
+            else if (v.equals(mCustomButton)) {
+                alertTitle = getString(R.string.alert_custom_title);
+
                 numberPickerBuilder
                         .minValue(1)
                         .maxValue(50)
@@ -66,7 +75,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             picker = numberPickerBuilder.build();
         }
 
-        new AlertDialog.Builder(this).setView(picker).show();
+        new AlertDialog.Builder(this)
+                .setTitle(alertTitle)
+                .setView(picker)
+                .setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Snackbar.make(findViewById(R.id.main_container), getString(R.string.picker_value, picker.getValue()), Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .show();
     }
 
     @Override
